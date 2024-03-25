91a32553d6f984cfc31d543b7bb19fc338d380c4
mYuking <kingyukingyouking@gmail.com>
2024-03-25 15:27:40 +0700

    pesan

---
+++ Semester2/Praktikum6/insert.php
@@ -0,0 +1,21 @@
+<?php 
+
+    include 'koneksi.php';
+    $nama = $_POST['nama'];
+    $nim = $_POST['nim'];
+    $jurusan = $_POST['jurusan'];
+    $semester = $_POST['semester'];
+
+    $query = "INSERT INTO mahasiswa (nama, nim, jurusan, semester)
+             VALUES ('$nama', '$nim', '$jurusan', '$semester') ";
+
+if($conn->query($query) === TRUE){
+    header("Location: index.php");
+} else {
+    echo "EROR:" . $query . "<br>" . $conn->eror;
+}
+
+$conn->close();
+
+
+?>
